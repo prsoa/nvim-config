f66c10cbebedde194932f259ce598ab8efdcfa81
Paulo Azevedo <paulo.azevedo@bolttech.io>
2024-10-17 12:13:34 +0100

    Fixed LSP diagnostic keybinds.

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -14,8 +14,8 @@ end
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<space>n", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "<space>p", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>p", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<space>n", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
